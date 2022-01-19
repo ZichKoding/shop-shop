ad606fc98015795b676f51c71058e531a483e943
Chris Zichko <chriszichkocoding@gmail.com>
2022-01-18 22:02:22 -0500

    Added Cart to the shop-shop

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -5,6 +5,7 @@ import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import { useStoreContext } from '../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../utils/actions';
+import Cart from '../components/Cart';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
@@ -51,6 +52,7 @@ function Detail() {
         </div>
       ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
+      <Cart />
     </>
   );
 }
